a2ebd9734028b4ce5be3e3e86a09a0d12161082d
KokumaiLuis <85628972+KokumaiLuis@users.noreply.github.com>
2021-09-13 22:27:13 -0300

    Filtro de data aplicado
    O filtro das datas foi aplicado, foi desenvolvido o print() de algumas informações

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import pyperclip
 import pandas as pd
 import datetime
 
-df = pd.read_excel(
-        r'C:\Users\T460s\Documents\GitHub\projeto_python_envio_relatorios_automatizados/Vendas - Dez.xlsx')
-print(pd.to_datetime(df['Data'], format='%b %d, %Y'))
-
 print(20 * '=', 'Relatório de Vendas', 20 * '=')
 while True:
     opc = int(input('1 - Resumo de Vendas\n2 - Relatório de Vendas via e-Mail\n'))
@@ -18,6 +14,7 @@ while True:
 
 if opc == 1:
     print()
+    #Lembrar! A solicitação deve ser feita com try e catch.
     dataini = datetime.datetime.strptime(str(input('Digite a data inicial do período que gostaria de analisar'
                                                    '(dd/mm/aaaa):\n')), '%d/%m/%Y')
     datafim = datetime.datetime.strptime(str(input('Digite a data final do período que gostaria de analisar'
@@ -26,14 +23,28 @@ if opc == 1:
     df = pd.read_excel(
         r'C:\Users\T460s\Documents\GitHub\projeto_python_envio_relatorios_automatizados/Vendas - Dez.xlsx')
     df_datas = pd.to_datetime(df['Data'], format='%b %d, %Y')
-    df_formatado = df.loc[dataini <= df_datas <= datafim]
 
-    print(df_formatado)
+    apos_dataini = df['Data'] >= dataini
+    antes_datafim = df['Data'] <= dataini
+    df_formatado = apos_dataini & antes_datafim
+    datas_filtradas = df.loc[df_formatado]
 
-    #df_formatado = df[['Código Venda', 'Data', 'ID Loja', 'Produto', 'Quantidade', 'Valor Unitário', 'Valor Final']]
 
-    print(10 * '-', 'Resumo Geral de Vendas', 10 * '-')
+    print(20 * '-', 'Resumo Geral de Vendas', 20 * '-')
+    print(f'Período: {dataini}', ' ~ ', f'{datafim}')
+    print()
 
+    faturamento = datas_filtradas['Valor Final'].sum()
+    qtd_prod = datas_filtradas['Quantidade'].sum()
+    maisvend = datas_filtradas[['Produto', 'Quantidade']].groupby('Produto').sum()
+    maisvend1 = maisvend.loc[maisvend['Quantidade'] == maisvend['Quantidade'].max()].reset_index()
+
+    print(f'Faturamento Total: R${round(faturamento, 2)}')
+    print(f'Quantidade de produtos vendidos: {qtd_prod}')
+    print()
+    print('Produto mais vendido:')
+    print(maisvend1['Quantidade'][0])
+    print(maisvend1['Produto'][0])
 
 '''pyautogui.PAUSE = 1
 
@@ -51,25 +62,10 @@ pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
 time.sleep(15)'''
 
-df = pd.read_excel(r'C:\Users\T460s\Documents\GitHub\projeto_python_envio_relatorios_automatizados/Vendas - Dez.xlsx')
-produtovalor = df[['Produto', 'Valor Final']]
-#print(df)
-
-faturamento = df['Valor Final'].sum()
-qtd_prod = df['Quantidade'].sum()
-
-maisvend = df[['Produto', 'Quantidade']].groupby('Produto').sum()
-maisvend1 = maisvend.loc[maisvend['Quantidade'] == maisvend['Quantidade'].max()].reset_index()
-
-
 #produto = ''
 
 '''for c, v in enumerate(maisvend):
     print(c)'''
-
-print('Produto mais vendido:')
-print(maisvend1['Quantidade'][0])
-print(maisvend1['Produto'][0])
 
 #print(produtovalor)
 
